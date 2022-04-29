5974baf039ad0a5d34ba65b037e1ba3ab541e56c
Ikenna <ikennaagada@gmail.com>
2022-04-29 14:46:42 +0100

    api websocket

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+const server = require("http").createServer(app);
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Big Fact Hunt API!')
 });
 
-module.exports = app;
+module.exports = server;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,35 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const app = require('./app');
-
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}!`)
+const server = require('./app');
+const io = require("socket.io")(server, {
+    cors: {
+      origin: '*',
+    }
 });
+
+const port = process.env.PORT || 5001;
+server.listen(port, () => console.log(`Express is running on port ${port}`));
+
+io.on('connection', socket => {
+    console.log("New connection has been made..."); // runs when client first connects
+
+    // get total number of client connections
+    const participantCount = io.engine.clientsCount;
+
+    // send event only to new connecting client
+    socket.emit('admin-message', 'Hi there, new fact hunter!');
+    // send event to all other clients (not new connecting client)
+    socket.broadcast.emit('admin-message', `A new fact hunter has arrived!`);
+    // send event to all clients
+    io.emit('admin-message', `There ${participantCount === 1 ? "is 1 fact hunter": `are ${participantCount} fact hunters`} here now!`);
+
+    io.emit('client-number',participantCount);
+    socket.on("disconnect", socket => { // runs when client disconnects
+        console.log("Goodbye...");
+    });
+});
+
+// server.listen(port, () => {
+//     console.log(`Open for play on port ${port}!`)
+// });
