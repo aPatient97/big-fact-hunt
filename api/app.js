4b83270e4c992a00c368c8f625bd9f8da654fc86
Ikenna <ikennaagada@gmail.com>
2022-04-29 11:57:16 +0100

    added basic app and server setup

---
+++ api/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Big Fact Hunt API!')
+});
+
+module.exports = app;
