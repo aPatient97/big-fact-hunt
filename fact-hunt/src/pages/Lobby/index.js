d6f5f180aa6b430f03fca021b296bc8e9e9368b5
Ikenna <ikennaagada@gmail.com>
2022-05-05 19:31:12 +0100

    chat box resets after submit

--- fact-hunt/src/pages/Lobby/index.js
+++ fact-hunt/src/pages/Lobby/index.js
@@ -9,6 +9,7 @@ const Lobby = ({socket}) => {
   const { change_url, user_data } = useSelector((state) => state);
   const [logs, setLogs] = useState(['']);
   const [ message, setMessage ] = useState(['']);
+  const [ chatInput, setChatInput ] = useState('');
   const [ quizStart, setQuizStart ] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -51,16 +52,16 @@ const Lobby = ({socket}) => {
     )
   }
 
-  const sendMessage = (e) => {
-    e.preventDefault();
-    let msg = e.target.chat.value;
+  const sendMessage = () => {
+    let msg = chatInput;
     let userMsg = `You: ${msg}`;
     setMessage(prevState => [...prevState, userMsg ]);
     socket.emit('new-msg', (msg) )
     socket.on('new-msg', (msg) => {
       setMessage(prevState => [...prevState, msg]);
       console.log(msg);
-    })
+    });
+    setChatInput('');
   };
 
   const startQuiz = () => {
@@ -83,13 +84,13 @@ const Lobby = ({socket}) => {
      <Rules />
       <div className="flex-center">   
             <button id={quizStart || user_data.creator ? 'start-btn' : 'hide'} onClick={startQuiz}>Start Quiz</button>
-            <form onSubmit={sendMessage}>
+            {/* <form onSubmit={e => e.preventDefault()} > */}
               <div id="flex-column">
                 <label htmlFor="chat">Chat</label>
-                <input type="text" name="chat" id="chat" placeholder="Say something nice..." autocomplete="off"></input>
+                <input type="text" name="chat" id="chat" placeholder="Say something nice..." autoComplete="off" value={chatInput} onChange={ e => setChatInput(e.target.value)} ></input>
               </div>
-              <button type='submit' id="chat-btn">Chat</button>
-            </form>
+              <button onClick={sendMessage} type='submit' id="chat-btn">Chat</button>
+            {/* </form> */}
       </div>
       <div id="chat-log-container">
         <div id="room-log">
