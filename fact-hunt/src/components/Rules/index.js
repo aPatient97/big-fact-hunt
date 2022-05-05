c7ed3f582fa7baabd333a3662e06ec0ea5ff80f0
Alex Patient <apatient97@gmail.com>
2022-05-05 17:11:24 +0100

    lobby styling

--- fact-hunt/src/components/Rules/index.js
+++ fact-hunt/src/components/Rules/index.js
@@ -6,9 +6,9 @@ const Rules = () => {
     <div className='rules'>
         <h2>How It's Going Down:</h2>
         <div className='rules-main'>
-            <h3>Welcome Big Fact Hunters! Are you ready?</h3>
+            <h3>Welcome Big Fact Hunters! Are you <span id="ready">ready?</span></h3>
             <p>The faster you answer your questions, the more points you get!</p>
-            <p>Compete against your friends to see who is the king or queen of the Big Fact Hunts</p>
+            <p>Compete against your friends to see who is the king or queen of the Big Fact Hunts</p>           
         </div>
     </div>
   )
--- fact-hunt/src/pages/Lobby/index.js
+++ fact-hunt/src/pages/Lobby/index.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Rules from '../../components/Rules';
+import './style.css'
 
 const Lobby = () => {
 
   const navigate = useNavigate();
 
   return (
-    <>  
-        <div>
-            Lobby
+    <>   
+     <span id='lobby-h1'><h1>Lobby</h1></span>
+     <Rules />
+      <div className="flex-center">   
+            <button id='start-btn' onClick={() => navigate('/quiz')}>Start Quiz</button>
+            <div id="flex-column">
+              <label htmlFor="chat">Chat</label>
+              <input type="text" name="chat" id="chat" placeholder="Say something nice..." autocomplete="off"></input>
+            </div>
+            <button type='submit' id="chat-btn">Chat</button>
+      </div>
+      <div id="chat-log-container">
+        <div id="room-log">
+            <h3>Room log</h3> 
+            <div>Insert log feed here you fact hunt</div>
         </div>
-        <div>   
-            {/* users displayed */}
-        </div>    
-        <div>
-            <button onClick={() => navigate('/quiz')}>Start Quiz</button>
+        <div id="room-chat">   
+            <h3>Room chat</h3>
+            <div>Insert chat feed here you fact hunt</div>
         </div>
-        <Rules />
+      </div>
     </>
   )
 }
